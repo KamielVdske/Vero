da921d96c6497e5267f242503828e773609b6d52
KamielVdske <kamielvdstraeten@icloud.com>
2025-02-26 19:08:29 +0100

    added Tauri with React

---
+++ Vero/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <div className="title">
+        <h1 className="title__title">Welcome to Vero</h1>
+        <h2 className="title__subtitle">A lightweight Apple Music client!</h2>
+      </div>
+    </main>
+  );
+}
+
+export default App;
